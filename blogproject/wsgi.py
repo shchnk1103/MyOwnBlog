b54ceca7a991697fa1d0eda49456c80110fe9216
oasis <761962425@qq.com>
2020-06-28 21:26:59 +0800

    只是用heroku的自动部署

--- blogproject/wsgi.py
+++ blogproject/wsgi.py
@@ -12,6 +12,6 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE',
-                      'blogproject.settings.production')
+                      'blogproject.settings')
 
 application = get_wsgi_application()
