76da79967c21954f8b9ddccd7237efac2c44b20d
oasis <761962425@qq.com>
2020-06-28 21:21:47 +0800

    自动部署

--- blogproject/settings/common.py
+++ blogproject/settings/common.py
@@ -13,23 +13,12 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(os.path.dirname(
+    os.path.dirname(os.path.abspath(__file__))))
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '4$%ag+-!zb9dn$%z41#_hzh$)6rxd9kxs04qft+nh26o4%cv2z'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = ['127.0.0.1',
-                 '.bbblog.website',
-                 'localhost',
-                 'bbblogwb.herokuapp.com']
-
 
 # Application definition
 
