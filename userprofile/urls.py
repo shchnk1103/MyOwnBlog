1d87d71383c2e9b2cc1eb61a67cc19506ab20b58
oasis <761962425@qq.com>
2020-07-02 12:56:06 +0800

    增加注册功能

--- userprofile/urls.py
+++ userprofile/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import user_login, user_logout
+from .views import user_login, user_logout, user_register
 
 
 app_name = 'userprofile'
 
 urlpatterns = [
     path('login/', user_login, name='login'),
-    path('logout/', user_logout, name='logout')
+    path('logout/', user_logout, name='logout'),
+    path('register/', user_register, name='register'),
 ]
